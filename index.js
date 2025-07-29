8057cc30fb7a7054b9842fcd7483f81600b5f5a5
Dianelito <95977637+Dianelito@users.noreply.github.com>
2025-07-29 13:38:57 +0000

    Try to fix in a preview mode in Vercel

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const port = 3000;
 app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use('/textures', express.static('public/textures'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/textures', express.static(path.join(__dirname, 'public/textures')));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
